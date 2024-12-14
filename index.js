f3172f68b145e4785a307b3688b857a52c61a947
VANSH PRATAP SINGH <vanshpratapsingh1121@gmail.com>
2024-12-15 00:27:28 +0530

    githhud data

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData = {
+    "message": "Not Found",
+    "documentation_url": "https://docs.github.com/rest",
+    "status": "404"
+  }
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -19,6 +25,9 @@ app.get('/login', (req,res) => {
 })
 app.get('/youtube', (req,res) => {
     res.send('<h2>chai aur code</h2>')
+})
+app.get('/github', (req, res)=> {
+    res.json(githubData)
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
